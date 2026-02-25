d9e8d22904d4a19fd6bd9e2a707209f72836b524
TS <tinusunny04@gmail.com>
2026-02-25 12:22:26 +0530

    feat(auth): implement role-based access control middleware

---
+++ middlewares/roleMiddleware.js
@@ -0,0 +1,11 @@
+const roleMiddleware = (role)=>{
+
+    return(req,res,next)=>{
+        if(req.payload.role !== role){
+            return res.status(403).json({message:"Access denied"})
+        }
+        next();
+    }
+}
+
+module.exports=roleMiddleware;
